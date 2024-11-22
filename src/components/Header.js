b10338ac7ac645f27ec0be5afd0874b1c328ed37
Kiran Pariti <downtowndynamitz@gmail.com>
2024-11-22 16:36:42 -0600

    styles and header nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,18 +8,20 @@ const Header = () => {
             <div className="banner">
                 <img src="/assets/images/banner.png" alt="Cricket Banner" className="banner-image" />
                 <div className="overlay">
-                    <img src="/assets/images/current.jpg" alt="Downtown Dynamitz Logo" className="logo" />
-                    <h1>Downtown Dynamitz Cricket Club</h1>
+                    <div className="logo-title">
+                        <img src="/assets/images/current.jpg" alt="Downtown Dynamitz Logo" className="logo" />
+                        <h1>Downtown Dynamitz Cricket Club</h1>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/team">Team</Link></li>
+                            <li><Link to="/schedule">Schedule</Link></li>
+                            <li><Link to="/contact">Contact</Link></li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/team">Team</Link></li>
-                    <li><Link to="/schedule">Schedule</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                </ul>
-            </nav>
         </header>
     );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,32 +3,39 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="home">
-            <div class="photo-gallery">
+            <div className="photo-gallery">
                 <h3>Photo Gallery</h3>
-                <img src="/assets/images/teamupdates/combined.jpg" alt="Downtown Dynamitz Logo" className="" />
-                <img src="/assets/images/teamupdates/mom.jpg" alt="Downtown Dynamitz Logo" className="" />
+                <img src="/assets/images/teamupdates/combined.jpg" alt="Downtown Dynamitz Logo" />
+                <img src="/assets/images/teamupdates/mom.jpg" alt="Downtown Dynamitz Logo" />
+            </div>
+            <p><h3>Upcoming Matches</h3></p>
+            <div className="upcoming-matches">
+                
+                
+                <div className="match-card hupl">
+                    <p><strong>HUPL Match Date:</strong> Nov 23, 2024 at 2:00 PM</p>
+                    <p><strong>Opponent:</strong> Huskies</p>
+                    <p><strong>Location:</strong> Badri Stadium</p>
+                </div>
+                
+                <div className="match-card bth">
+                    <p><strong>BTH Match Date:</strong> Dec 7, 2024 at 7:30 AM</p>
+                    <p><strong>Opponent:</strong> Houston Scorpions</p>
+                    <p><strong>Location:</strong> BTH Grounds</p>
+                </div>
             </div>
 
-            <div class="upcoming-matches">
-                <h3>Upcoming Matches</h3>
-                
-                <p><strong>HUPL Match Date:</strong> Nov 23, 2024 at 2:00 PM - <strong>Opponent:</strong> Huskies - <strong>Location:</strong> Badri Stadium</p>
-                
-                <p><strong>BTH Match Date:</strong> Dec 7th, 2024 at 7.30 AM - <strong>Opponent:</strong> Houston Scorpions - <strong>Location:</strong> BTH Grounds</p>
-                
-                    
-            </div>
-            <div class="latest-news">
+            <div className="latest-news">
                 <h3>Latest News</h3>
                 <p>Downtown Dynamitz secures a thrilling victory against the Oldfarm Rangers!</p>
                 <p>Star player <strong>Kiran Boddupalli</strong> achieves a personal best with 33 runs.</p>
             </div>
 
-            <div class="weeklykey-players">
+            <div className="weeklykey-players">
                 <h3>Meet Our Key Players for this week</h3>
-                    <p><a href="/team/kiranp">Kiran Kumar Pariti - Captain</a></p>
-                    <p><a href="/team/kiranb">Kiran Boddupalli - Batsman</a></p>
-                    <p><a href="/team/ashok">Ashok Kolluru - Bowler</a></p>
+                <p><a href="/team/kiranp">Kiran Kumar Pariti - Captain</a></p>
+                <p><a href="/team/kiranb">Kiran Boddupalli - Batsman</a></p>
+                <p><a href="/team/ashok">Ashok Kolluru - Bowler</a></p>
             </div>
         </div>
     );
